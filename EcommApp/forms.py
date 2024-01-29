b4fd54a1d0e92a3758479598c8918055780d6a0a
Prerna3 <prerna.ranjan@slrtce.in>
2024-01-29 21:49:52 +0530

    created insertProduct by admin

--- EcommApp/forms.py
+++ EcommApp/forms.py
@@ -1,8 +1,16 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Product
+from django.forms import ModelForm
+
 
 class CreateUserForm(UserCreationForm):
-  class Meta:
-    model = User
-    fields = ['username','email','password1','password2']
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
 
+
+class AddProduct(ModelForm):
+    class Meta:
+        model = Product
+        fields = "__all__"
